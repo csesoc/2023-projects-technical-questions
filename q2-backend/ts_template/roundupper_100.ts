f59eccb975d0dfb447c67ac768e82f3f2bd076ab
Varun Sethu <varun.sethu@outlook.com>
2022-12-26 13:44:00 +1100

    added BE question + scaffold

---
+++ q2-backend/ts_template/roundupper_100.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+
+// location is the simple (x, y) coordinates of an entity within the system
+// spaceCowboy models a cowboy in our super amazing system
+// spaceAnimal models a single animal in our amazing system
+type location = { x: number, y: number };
+type spaceCowboy = { name: string, lassoSize: number };
+type spaceAnimal = { type: "pig" | "cow" | "flying_burger" };
+
+// spaceEntity models an entity in the super amazing (ROUND UPPER 100) system
+type spaceEntity =
+    | { type: "space_cowboy", metadata: spaceCowboy, location: location }
+    | { type: "space_animal", metadata: spaceAnimal, location: location };
+
+
+// === ADD YOUR CODE BELOW :D ===
+
+// === ExpressJS setup + Server setup ===
+const spaceDatabase = [] as spaceEntity[];
+const app = express();
+
+// the createEntity endpoint adds an entity to your global space database
+app.post('/createEntity', (req, res) => {
+    // TODO: fill me in
+});
+
+// lasooable returns all the space animals a space cowboy can lasso given their name
+app.get('/lassoable', (req, res) => {
+    // TODO: fill me in
+})
+
+app.listen(8080);
