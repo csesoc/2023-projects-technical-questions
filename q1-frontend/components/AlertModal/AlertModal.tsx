f9d15d5f8adb2255654930d20f3d8759a12689bf
Peter Derias <peter_derias@hotmail.com>
2022-12-27 18:49:24 +1100

    added tests to question to guide solution

--- q1-frontend/components/AlertModal/AlertModal.tsx
+++ q1-frontend/components/AlertModal/AlertModal.tsx
@@ -6,13 +6,13 @@ interface AlertModalProps {
 }
 
 export default function AlertModal({useContents}: AlertModalProps) {
-  const onSubmitEvent = (e: FormEvent<HTMLFormElement>) => {
+  function onSubmitEvent(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    // hint: the alert given is at e.target[0].value - ignore typescript being annoyings
+    // hint: the alert given is at (e.target as any).elements[0].value - ignore typescript being annoying
     console.log((e.target as any)[0].value);
   }
   return (
-    <form onSubmit={onSubmitEvent}>
+    <form data-testid='form' onSubmit={onSubmitEvent}>
       <label> Add new Alert: </label>
       <input type='text' id='alert' name='alert' />
       <button type='submit'> Add </button>
