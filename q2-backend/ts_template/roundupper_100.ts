b6e0a6f991a33af5c9299438c9d72a2b52b74df5
Jeremy <jeremy.le420@gmail.com>
2023-01-05 17:44:16 +1100

    Completed q2-backend in TypeScript

--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -14,19 +14,61 @@ type spaceEntity =
 
 
 // === ADD YOUR CODE BELOW :D ===
+type entityCowboy = { type: "space_cowboy", metadata: spaceCowboy, location: location };
+type entityAnimal = { type: "space_animal", metadata: spaceAnimal, location: location };
+// NOTE: With the above added you can change type spaceEntity using these 
+// Optionally remove these and use different method in getLassoable (type checking)
+
+type animal = spaceAnimal & { location: location }
 
 // === ExpressJS setup + Server setup ===
 const spaceDatabase = [] as spaceEntity[];
 const app = express();
+app.use(express.json());
 
 // the POST /entity endpoint adds an entity to your global space database
 app.post('/entity', (req, res) => {
-    // TODO: fill me in
+    const { entities } = req.body;
+    spaceDatabase.push(...entities);
+    res.json();
 });
 
 // lasooable returns all the space animals a space cowboy can lasso given their name
 app.get('/lassoable', (req, res) => {
-    // TODO: fill me in
-})
+    const cowboyName = req.query.cowboy_name as string;
+    res.json(getLassoable(cowboyName));
+});
+
+// Helper function to find all space animals within lasso range
+const getLassoable = (cowboyName: string): { space_animals: animal[] } => {
+    const cowboy = spaceDatabase.find(
+        c => c.type === "space_cowboy" && c.metadata.name === cowboyName
+    ) as entityCowboy;
+
+    if (!cowboy) return { space_animals: [] };
+
+    const lassoLength = cowboy.metadata.lassoLength;
+    const { x, y } = cowboy.location; 
+    
+    // Outter box optimisation 
+    const possibleAnimals = spaceDatabase.filter(
+        e => e.type === "space_animal" && 
+        e.location.x <= x + lassoLength && e.location.x >= x - lassoLength &&
+        e.location.y <= y + lassoLength && e.location.y >= y - lassoLength
+    ) as entityAnimal[];
+
+    // Finds animals with Pythagorean distance from animal to cowboy <= lasso length 
+    const maxDistance = lassoLength * lassoLength;
+    const result = [] as animal[];
+    possibleAnimals.forEach(a => {
+        const dxSquared = Math.pow(Math.abs(x - a.location.x), 2);
+        const dySquared = Math.pow(Math.abs(y - a.location.y), 2);
+        if (dxSquared + dySquared <= maxDistance) {
+            result.push({ type: a.metadata.type, location: a.location });
+        }
+    });
+
+    return { space_animals: result };
+};
 
 app.listen(8080);
