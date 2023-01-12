dccdf2243d1a56939d9833d28234abc4f705b776
Hanyuan Li <hanyuan.work@gmail.com>
2023-01-13 08:05:05 +1100

    fix(q2): update metadata

--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -17,54 +17,15 @@ type spaceEntity =
 // === ExpressJS setup + Server setup ===
 const spaceDatabase = [] as spaceEntity[];
 const app = express();
-app.use(express.json());
 
 // the POST /entity endpoint adds an entity to your global space database
 app.post("/entity", (req, res) => {
-  const { entities } = req.body;
-  entities.forEach((entity: spaceEntity) => {
-    spaceDatabase.push(entity);
-  });
-  res.json({});
+  // TODO: fill me in
 });
 
 // lasooable returns all the space animals a space cowboy can lasso given their name
 app.get("/lassoable", (req, res) => {
-  const { cowboy_name } = req.body;
-  let lassoLength: number;
-  let cowboyLocation: location;
-
-  // Loops through database to find lassoLength and
-  // location for requested cowboy
-  spaceDatabase.forEach((entity: spaceEntity) => {
-    if (
-      entity.type === "space_cowboy" &&
-      entity.metadata.name === cowboy_name
-    ) {
-      lassoLength = entity.metadata.lassoLength;
-      cowboyLocation = entity.location;
-    }
-  });
-
-  const space_animals = [] as spaceEntity[];
-
-  // Loop through space animals and add the appropriate ones
-  // to array which will be sent back
-  spaceDatabase.forEach((entity: spaceEntity) => {
-    if (entity.type === "space_animal") {
-      let animalLocation = entity.location;
-      let equation =
-        Math.pow(cowboyLocation.x - animalLocation.x, 2) +
-        Math.pow(cowboyLocation.y - animalLocation.y, 2);
-
-      const distance = Math.sqrt(equation);
-
-      // Add entity to result array if it lies in the cowboys reach
-      if (distance <= lassoLength) space_animals.push(entity);
-    }
-  });
-
-  res.json({ space_animals });
+  // TODO: fill me in
 });
 
 app.listen(8080);
